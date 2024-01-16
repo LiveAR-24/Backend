f73726c471104ede0b97d3d6814484f400ab311f
su-jin02 <hera4224@naver.com>
2024-01-16 09:30:01 +0900

    프로젝트 세팅

---
+++ src/main/java/com/livear/LiveAR/LiveArApplication.java
@@ -0,0 +1,13 @@
+package com.livear.LiveAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveArApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveArApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livear/LiveAR/LiveArApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livear.LiveAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+class LiveArApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
