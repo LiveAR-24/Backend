c18a4f84c3cb305a2f812f8fdbe5a18ee3d1b5d2
su-jin02 <hera4224@naver.com>
2024-01-29 09:36:24 +0900

    style:이름 변경

--- src/main/java/com/livear/LiveAR/service/AdminService.java
+++ src/main/java/com/livear/LiveAR/service/AdminService.java
@@ -16,8 +16,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Optional;
-
 import static com.livear.LiveAR.domain.UserRole.ROLE_ADMIN;
 
 @Service
