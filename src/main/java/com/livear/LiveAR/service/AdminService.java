a4a303f2b7ac614578b528c07562450c041c93a2
su-jin02 <hera4224@naver.com>
2024-01-30 09:38:40 +0900

    fix: 도안 생성 return 값 변경

--- src/main/java/com/livear/LiveAR/service/AdminService.java
+++ src/main/java/com/livear/LiveAR/service/AdminService.java
@@ -31,11 +31,11 @@ public class AdminService {
     /**
      * 도안 등록
      */
-    public String createDrawing(AdminReq.CreateDrawing createDrawing, MultipartFile image) {
+    public Long createDrawing(AdminReq.CreateDrawing createDrawing, MultipartFile image) {
         String uploadFileUrl = s3Uploader.uploadFile(image);
         Drawing drawing = createDrawing.toEntity(uploadFileUrl);
         drawingRepository.save(drawing);
-        return "도안 생성 성공";
+        return drawing.getDrawingId();
     }
 
     /**
